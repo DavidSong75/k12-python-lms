3c91b5279ea813f9bc9e1a2cf3a317b273670cf4
Your Name <you@example.com>
2026-02-19 00:22:33 +0900

    Test: add /test route to debug Vercel deployment

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: 20, color: "white" }}>
+            <h1>Vercel Deployment Test</h1>
+            <p>If you see this, routing works!</p>
+        </div>
+    );
+}
